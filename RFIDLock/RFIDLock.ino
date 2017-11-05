8b13fa8d37d752c7b1e0ecdd8565101aa862fa1f
Mads Mogensen <lort256c@gmail.com>
2017-11-05 18:38:14 +0100

    The user can now add up to 3 cards

--- RFIDLock/RFIDLock.ino
+++ RFIDLock/RFIDLock.ino
@@ -6,14 +6,28 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
+#define KeySize 10
+#define MaxKnownKeys 3
+
+#define ACCESS_DELAY 3000
+
+#define NEWKEYBUTTON_PIN 5
+
+#define GREEN_PIN 6
+#define RED_PIN 7
+
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 mfrc522(SS_PIN, RST_PIN);	// Create MFRC522 instance.
 
-bool firstTime = true;
-byte test[10];
+bool allowNewKey = false;
+byte knownKeysAmount = 0;
+byte KnownKeys[MaxKnownKeys][KeySize];
 
 void setup() {
+	pinMode(NEWKEYBUTTON_PIN, INPUT);
+	pinMode(GREEN_PIN, OUTPUT);
+	pinMode(RED_PIN, OUTPUT);
 	Serial.begin(9600);	// Initialize serial communications with the PC
 	SPI.begin();			// Init SPI bus
 	mfrc522.PCD_Init();	// Init MFRC522 card
@@ -21,6 +35,13 @@ void setup() {
 }
 
 void loop() {
+
+	if (digitalRead(NEWKEYBUTTON_PIN))
+	{
+		allowNewKey = true;
+		delay(ACCESS_DELAY);
+	}
+
 	// Look for new cards
 	if (!mfrc522.PICC_IsNewCardPresent()) {
 		return;
@@ -31,30 +52,56 @@ void loop() {
 		return;
 	}
 	
-	if (firstTime)
+	if (allowNewKey)
 	{
-		for (int i = 0; i < 10; i++)
+		for (int i = 0; i < KeySize; i++)
 		{
-			test[i] = mfrc522.uid.uidByte[i];
+			KnownKeys[knownKeysAmount][i] = mfrc522.uid.uidByte[i];
 		}
-		firstTime = false;
-		delay(5000);
+		allowNewKey = false;
+		knownKeysAmount++;
+		digitalWrite(GREEN_PIN, HIGH);
+		digitalWrite(RED_PIN, HIGH);
+		delay(ACCESS_DELAY);
+		digitalWrite(RED_PIN, LOW);
+		digitalWrite(GREEN_PIN, LOW);
 		return;
 	}
 	
 	else
 	{
-		for (int i = 0; i < 10; i++)
+		bool test = false;
+		for (byte x = 0; x < knownKeysAmount; x++)
 		{
-			if (test[i] != mfrc522.uid.uidByte[i])
+			for (byte i = 0; i < KeySize; i++)
 			{
-				Serial.println("Access Denied");
-				delay(5000);
-				return;
+				if (KnownKeys[x][i] != mfrc522.uid.uidByte[i])
+				{
+					goto label;
+					
+				}
 			}
+			test = true;
+			break;
+		label:;
 		}
-		Serial.println("Access Granted");
-		delay(5000);
+
+		if (test)
+		{
+			Serial.println("Access Granted");
+			digitalWrite(GREEN_PIN, HIGH);
+			delay(ACCESS_DELAY);
+			digitalWrite(GREEN_PIN, LOW);
+		}
+		else
+		{
+			Serial.println("Access Denied");
+			digitalWrite(RED_PIN, HIGH);
+			delay(ACCESS_DELAY);
+			digitalWrite(RED_PIN, LOW);
+		}
+
+
 	}
 
 
